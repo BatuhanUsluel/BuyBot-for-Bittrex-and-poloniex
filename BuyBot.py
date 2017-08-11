bda11f35b2683d078f55c02602ed43d4be144166
BatuhanUsluel <Batuhan.Usluel@gmail.com>
2017-08-11 15:12:23 -0400

    Added support for poloniex, improved placing sell order in second try

--- BuyBot.py
+++ BuyBot.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 #Imports
+from poloniex import poloniex
 from bittrex import bittrex
 from time import sleep
+import sys
 
 #Print
 print "-----------------------------------------"
@@ -10,17 +12,27 @@ print "Press CTRL+C at any time to close and cancel the program"
 print "Coded by Batuhan"
 print "-----------------------------------------"
 
-#ApiKeys
-api = bittrex('bc3eb80b26694c0f8653c085ee9002f7','1de0219f7d8d4eafa68cbc33b08cf6e9')
+#Bittrex Api Keys
+api = bittrex('APIKEY','APISECRET')
 
+#Polo API Keys
+conn= poloniex('APIKEY','APISECRET')
 #Market to trade at
 trade = 'BTC'
 
 #----------------------------------------
 #INPUTS
 #----------------------------------------
+#Poloniex or Bittrex
+exchange=float(raw_input("Enter 1 for bittrex, 2 for poloniex:		"))
+if (exchange!=1):
+	if (exchange!=2):
+		sys.exit('Exchange number {0} not valid. Please enter 1 for bittrex, 2 for poloniex'.format(exchange))
 #Bitcoin to spend
 bitcoin=float(raw_input("Bitcoin to Spend: "))
+
+#Time to Wait
+wait=int(raw_input("How many seconds to wait to check for sell order(incase the first time doesn't work(happens if the buy order isn't immediately filled)) in seconds(only integers):		"))
 
 #BuyingMultiplier
 buyingmultiplier=float(raw_input("Buy price multiplier: "))
@@ -30,17 +42,24 @@ sellingmultiplier=float(raw_input("Selling price multiplier: "))
 
 #Coint to Buy
 currency = raw_input("Coin: ")
+
+#Bittrex market
 market= '{0}-{1}'.format(trade,currency)
 
-#Getting Current Price
-summary=api.getmarketsummary(market)
-price = summary[0]['Last']
-print 'The price for {0} is {1:.8f} {2}.'.format(currency, price, trade)
+#Polo market
+pair= '{0}_{1}'.format(trade,currency)
 
-#Getting Lowest Price of Day
-#summary=api.getmarketsummary(market)
-#price = summary[0]['Low']
-#print 'The lowest price of the day for {0} is {1:.8f} {2}.'.format(currency, price, trade)
+#Getting Current Price
+if (exchange==1):
+	summary=api.getmarketsummary(market)
+	price = summary[0]['Last']
+	print 'The price for {0} at bittrex is {1:.8f} {2}.'.format(currency, price, trade)
+elif (exchange==2):
+	currentValues = conn.api_query("returnTicker")
+	price = float(currentValues[pair]["last"])
+	print 'The price for {0} at poloniex is {1:.8f} {2}.'.format(currency, price, trade)
+else:
+	sys.exit('Exchange number {0} not valid. Please enter 1 for bittrex, 2 for poloniex'.format(exchange))
 
 #Buying Multiplier
 buyprice = round(price*buyingmultiplier, 8)
@@ -49,18 +68,41 @@ buyprice = round(price*buyingmultiplier, 8)
 amount=round(bitcoin/buyprice, 8)
 
 #Buying
-print 'Buying {0} {1} for {2:.8f} {3}.'.format(amount, currency, buyprice, trade)
-api.buylimit(market, amount, buyprice)
+if (exchange==1):
+	print 'Buying {0} {1} @ Bittrex for {2:.8f} {3}.'.format(amount, currency, buyprice, trade)
+	api.buylimit(market, amount, buyprice)
+elif (exchange==2):
+	print 'Buying {0} {1} @ Poloniex for {2:.8f} {3}.'.format(amount, currency, buyprice, trade)
+	orderNumber=conn.buy(pair, buyprice, amount)
+else:
+	sys.exit('Exchange number {0} not valid. Please enter 1 for bittrex, 2 for poloniex'.format(exchange))
 
 #Multiplying the price by the multiplier
 sellprice = round(price*sellingmultiplier, 8)
 
 #Selling
-print 'Selling {0} {1} for {2:.8f} {3}.'.format(amount, currency, sellprice, trade)
-api.selllimit(market, amount, sellprice)
-	
-#Checking balance and reselling(Incase selling doesn't work)
-sleep(3)
-balance = api.getbalance(currency)
-if balance>0:
-	api.selllimit(market, balance, sellprice)
+if (exchange==1):
+	print 'Selling {0} {1} @ Bittrex for {2:.8f} {3}.'.format(amount, currency, sellprice, trade)
+	api.selllimit(market, amount, sellprice)
+elif (exchange==2):
+	print 'Selling {0} {1} @ Poloniex for {2:.8f} {3}.'.format(amount, currency, sellprice, trade)
+	orderNumber=conn.sell(pair,sellprice,amount)
+else:
+	sys.exit('Exchange number {0} not valid. Please enter 1 for bittrex, 2 for poloniex'.format(exchange))
+
+#RePlace sell order(incase first time doesn't work)
+sleep(wait)
+print 'Rechecking balance for {0} and placing sell order @ {1}'.format(currency, sellprice)
+if (exchange==1):
+	bittrexbalance = api.getbalance(currency)
+	print 'Your Balance for {0} is {1}'.format(currency, bittrexbalance)
+	print 'Placing sell order @ {0} {1} for {2} {3}'.format(sellprice, trade, bittrexbalance, currency)
+elif (exchange==2):
+	allpolobalance = conn.api_query('returnBalances')
+	polobalance = allpolobalance[currency]
+	print 'Your Balance for {0} is {1}'.format(currency, polobalance)
+	print 'Placing sell order @ {0} {1} for {2} {3}'.format(sellprice, trade, polobalance, currency)
+	orderNumber=conn.sell(pair, sellprice, polobalance)
+else:
+        sys.exit('Exchange number {0} not valid. Please enter 1 for bittrex, 2 for poloniex'.format(exchange))
+
